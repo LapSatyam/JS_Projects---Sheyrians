c6887e22243856319581a27cf89fb964e06bcc12
Satyam Sharma <lapsatyam@gmail.com>
2026-06-07 13:44:55 +0530

    Made drag and drop feature complete

--- Kanban-Board/script.js
+++ Kanban-Board/script.js
@@ -1,13 +1,15 @@
-const tasks = document.querySelectorAll(".task");
+const Alltask = document.querySelectorAll(".task");
 const colum = document.querySelectorAll(".colum");
+const form = document.getElementById("form");
 
-tasks.forEach(task => {
+let dragElement = null;
+
+Alltask.forEach(task => {
     task.addEventListener("drag", (e) => {
-        // console.log("dragging", e);
+        dragElement = task;
 
     })
 });
-
 
 colum.forEach(col => {
     col.addEventListener("dragenter", (e) => {
@@ -23,6 +25,28 @@ colum.forEach(col => {
     });
     col.addEventListener("drop", (e) => {
         e.preventDefault();
+        col.appendChild(dragElement);
         col.classList.remove("hover");
     });
 });
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const taskTitle = document.getElementById("Inptitle").value.trim();
+    const taskDis  = document.getElementById("Inpdis").value.trim();
+    const tasks = document.getElementById("tasks");
+
+    tasks.innerHTML += `
+    <div class="task" draggable="true">
+            <div>
+              <h2 class="title">${taskTitle}</h2>
+              <p class="para">${taskDis}</p>
+            </div>
+            <button class="button">Delete</button>
+          </div>
+        </div>
+    `;
+
+    document.getElementById("taskModal").classList.add("hidden");
+})
